3669a08375949bb9d0dfcc20fbf5948c30f460b1
Aaron Maritz <maritz61@uw.edu>
2019-08-20 17:53:36 -0700

    1.4

---
+++ chapter_1/palindromePermutation.py
@@ -0,0 +1,27 @@
+# Aaron Maritz
+# Given a string, write a function to check if it is a permutation of a palindrome
+# Thoughts -> palindrome is same forwards and backwards
+# Only real thing to check -> if the count of a letter is even -> good
+# There can only be one character with an odd count, if 2 -> wont be allowed
+
+def isPalPerm(str) :
+    oddCount = 0
+    counts = {}
+    str = str.lower()
+    for char in str :
+        counts[char] = counts.get(char, 0) + 1
+    for char in counts :
+        if char != ' ' :
+            if counts[char] % 2 == 1 :
+                oddCount += 1
+    return oddCount <= 1
+
+if __name__ == '__main__' :
+    print("Testing isPalindromePermutation")
+    print(isPalPerm("Tact Coa"))
+    print(isPalPerm("ab    c"))
+    print(isPalPerm("a   a"))
+    print(isPalPerm(""))
+    print(isPalPerm("b"))
+    print(isPalPerm("bbaa"))
+    print(isPalPerm("b   a   b  z"))
