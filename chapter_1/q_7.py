0a284045c926b8ea0415b13ae530ef0b0f52c9ab
Aaron Maritz <maritz61@cs.washington.edu>
2019-08-24 22:47:30 -0700

    q_7 not done

--- chapter_1/q_7.py
+++ chapter_1/q_7.py
@@ -1,3 +1,21 @@
 # Aaron Maritz
 # Question 1.7
-# Fixed email shenanigans
+# Given an image represented by an NxN matrix, -> each pixel in the image is 4 byte
+# Rotate the image by 90 degress -> Do this in place.
+# So this is an N x N integer matrix
+#[0] -> [0]
+#[2,3] -> [5,2]
+#[5,4]    [4,3]
+
+# Algorithm -> x,y -> rows becomes columns
+# You just swap index's
+# Go layer by layer
+
+def rotate90(matrix) :
+    n = len(matrix) / 2
+    for level in range (n)
+        # Have the number of layers
+        start = level
+        end = n - 1 - level
+        for x in range (start, end)
+            offset = x - start
