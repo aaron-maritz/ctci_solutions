c55736c9b671791da3fa9907bc68bb9c7b40b5d7
Aaron Maritz <maritz61@uw.edu>
2019-08-24 23:02:50 -0700

    initial

---
+++ chapter_2/Node.java
@@ -0,0 +1,17 @@
+class Node {
+	Node next = null;
+	int data;
+	
+	public Node(int d) {
+		data = d;
+	}
+	
+	void appendToTail(int d) {
+		Node end = new Node(d);
+		Node n = this;
+		while (n.!= null) {
+			n = n.next;
+		}
+		n.next = end;
+	}
+}
