5b3bcd264c2591ddfa210622cbf639d41d0ae077
Aaron Maritz <maritz61@uw.edu>
2019-08-24 23:37:59 -0700

    Trying

--- chapter_2/Node.java
+++ chapter_2/Node.java
@@ -1,15 +1,19 @@
 class Node {
 	Node next = null;
 	int data;
-	
+
 	public Node(int d) {
 		data = d;
 	}
-	
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
 	void appendToTail(int d) {
 		Node end = new Node(d);
 		Node n = this;
-		while (n.!= null) {
+		while (n!= null) {
 			n = n.next;
 		}
 		n.next = end;
